d91956ec805e8879d44b8c0eb6886aca4fe2b96a
Mike Beliakovski <mike.beliakovski@gmail.com>
2023-06-12 14:58:50 +0300

    Added ToDoItemAdapter and ToDoItemViewHolder

---
+++ app/src/main/java/ru/mobiledevschool/todoapp/ToDoItemViewHolder.kt
@@ -0,0 +1,31 @@
+package ru.mobiledevschool.todoapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.mobiledevschool.todoapp.databinding.ToDoItemBinding
+
+class ToDoItemViewHolder(private val binding: ToDoItemBinding): RecyclerView.ViewHolder(binding.root) {
+    fun onBind(toDoItem: ToDoItem) {
+        binding.itemText.text = toDoItem.text
+        binding.checkBox.bindCompletionImage(toDoItem.completed, toDoItem.priority)
+    }
+}
+
+class ToDoItemAdapter(private val toDoItems: ArrayList<ToDoItem> ): RecyclerView.Adapter<ToDoItemViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ToDoItemBinding.inflate(layoutInflater,parent, false)
+        return ToDoItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ToDoItemViewHolder, position: Int) {
+        holder.onBind(toDoItems[position])
+    }
+
+    override fun getItemCount(): Int {
+        return toDoItems.size
+    }
+}
